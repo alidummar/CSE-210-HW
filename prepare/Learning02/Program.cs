0ff257f004a866b4fa712e08986b3da5c5d16fab
alidummar <alidummar@byui.edu>
2023-04-26 11:27:15 -0600

    new

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Project Manager";
+        job1._company = "Intel";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Intern";
+        job2._company = "INL";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Alexandria Dummar";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
